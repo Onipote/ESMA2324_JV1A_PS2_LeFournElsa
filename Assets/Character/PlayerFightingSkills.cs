b3df5e077c5752d3cef8da0fbf59e8671801ebe4
Onipote <elsa.lefourn16@gmail.com>
2024-05-15 12:29:58 +0200

    + Ability : Scratch Attack

--- Assets/Character/PlayerFightingSkills.cs
+++ Assets/Character/PlayerFightingSkills.cs
@@ -4,6 +4,18 @@ using UnityEngine;
 
 public class PlayerFightingSkills : MonoBehaviour
 {
+    private int facedDirection;
+    public GameObject rightHitbox;
+    public GameObject leftHitbox;
+
+    public PlayerMovements pm;
+
+    //Damages according to the attack
+    [SerializeField] private float baseScratch; //when walking + attack
+    [SerializeField] private float lightScratch; //when jumping + attack
+    [SerializeField] private float heavyScratch; //when running + attack
+    private float currentStrenght;
+
     void Start()
     {
         
@@ -11,6 +23,31 @@ public class PlayerFightingSkills : MonoBehaviour
 
     void Update()
     {
-        
+        if (pm.isGrounded && Input.GetKey(KeyCode.LeftShift) && Input.GetButtonDown("Fire1"))
+        {
+            baseS_Attack(facedDirection);
+        }
+        if (pm.isGrounded && Input.GetButtonDown("Fire1"))
+        {
+            lightS_Attack(facedDirection);
+        }
     }
+
+    public void baseS_Attack(int id)
+    {
+        baseScratch = UnityEngine.Random.Range(15,70);
+        Debug.Log("Base scratch : " + baseScratch);
+    }
+
+    public void lightS_Attack(int id)
+    {
+        lightScratch = UnityEngine.Random.Range(1, 30);
+        Debug.Log("Light scratch : " + lightScratch);
+    }
+    public void heavyS_Attack(int id)
+    {
+        heavyScratch = UnityEngine.Random.Range(45, 100);
+        Debug.Log("Heavy scratch : " + heavyScratch);
+    }
+
 }
